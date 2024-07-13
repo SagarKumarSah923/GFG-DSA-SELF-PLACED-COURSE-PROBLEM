fcdf399e10a6b401bcbfe73bc5a78595ea356194
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:16:06 +0530

    Update Multiplication Under Modulo.cpp

--- Multiplication Under Modulo.cpp
+++ Multiplication Under Modulo.cpp
@@ -1,5 +1,25 @@
-#include<isotream>
+#include <iostream>
 using namespace std;
-int main(){
+
+class Solution {
+public:
+    int multiplicationUnderModulo(long long a, long long b) {
+        int M = 1000000007;
+        a = a % M;
+        b = b % M;
+        return (a * b) % M;
+    }
+};
+
+int main() {
+    Solution solution;
     
+    long long a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    
+    int result = solution.multiplicationUnderModulo(a, b);
+    cout << "The multiplication under modulo is: " << result << endl;
+    
+    return 0;
 }
