a1b7a49a66cd02c70396dd4dc4ff286ff88e4d1b
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:07:09 +0530

    Update Countdigits.cpp

--- Countdigits.cpp
+++ Countdigits.cpp
@@ -1,5 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
-int main(){
+
+int countDigits(int x)
+{
+	int res = 0;
+
+	while(x > 0)
+	{
+			x = x / 10;
+
+			res++;
+	}	
+
+	return res;
+}
+
+int main() {
     
+    	int number = 789;
+    	
+    	cout<<countDigits(number);
+    	
+    	return 0;
 }
