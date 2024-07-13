a05b1b45352fb317aeebe6f6256505abf8424d3b
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:16:46 +0530

    Update PalindromeNumber.cpp

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -1,5 +1,28 @@
-#include<isotream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
-int main(){
+
+bool isPal(int n)
+{
+	int rev = 0;
+
+	int temp = n;
+	while(temp != 0)
+	{
+		int ld = temp % 10;
+
+		rev = rev * 10 + ld;
+
+		temp = temp / 10;
+	}	
+
+	return rev==n;
+}
+int main() {
     
+    	int number = 4553;
+    	
+    	cout<<(bool)isPal(number);
+    	
+    	return 0;
 }
