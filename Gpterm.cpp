22351460db963dc8e0e2615bac720d34d1dd5c21
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:12:29 +0530

    Update Gpterm.cpp

--- Gpterm.cpp
+++ Gpterm.cpp
@@ -1,5 +1,28 @@
-#include<isotream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
+
+class Solution {
+public:
+    double termOfGP(double A, double B, int N) {
+        return A * pow(B / A, N - 1);
+    }
+};
+
+int main() {
+    Solution solution;
     
+    double A, B;
+    int N;
+    cout << "Enter the first term (A): ";
+    cin >> A;
+    cout << "Enter the second term (B): ";
+    cin >> B;
+    cout << "Enter the term number (N): ";
+    cin >> N;
+    
+    double result = solution.termOfGP(A, B, N);
+    cout << "The " << N << "th term of the GP is: " << result << endl;
+    
+    return 0;
 }
