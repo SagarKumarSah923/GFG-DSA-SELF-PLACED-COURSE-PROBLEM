3063bf37e6b5e4d719e0616211e4e67ec0ddb98d
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:22:34 +0530

    Update Traling zeros to factorial.cpp

--- Traling zeros to factorial.cpp
+++ Traling zeros to factorial.cpp
@@ -1,5 +1,23 @@
-#include<isotream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
-int main(){
+
+ int countTrailingZeros(int n)
+{
+	int res = 0;
+
+	for(int i=5; i<=n; i=i*5)
+	{
+		res = res + (n / i);
+	}
+
+	return res;
+}
+int main() {
     
+    	int number = 251;
+    	
+    	cout<<countTrailingZeros(number);
+    	
+    	return 0;
 }
