97caec0087a76035ee4d569063520212b1dbb57a
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:10:45 +0530

    Update Gcd to Hcf ot two Number.cpp

--- Gcd to Hcf ot two Number.cpp
+++ Gcd to Hcf ot two Number.cpp
@@ -1,5 +1,48 @@
-#include<isotream>
+//Eucliden Algorithm
+#include <iostream>
+#include <limits.h>
 using namespace std;
-int main(){
+
+ int gcd(int a, int b)
+{
+	while(a!=b)
+	{
+		if(a > b)
+			a = a - b;
+		else
+			b = b - a; 
+	}
+
+	return a;
+}
+
+int main() {
     
+    	int a = 12, b = 15;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
+}
+
+//Optimized Eucliden Algorithm
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ int gcd(int a, int b)
+{
+	if(b==0)
+		return a;
+
+	return gcd(b, a % b);
+}
+
+int main() {
+    
+    	int a = 12, b = 15;
+    	
+    	cout<<gcd(a, b);
+    	
+    	return 0;
 }
