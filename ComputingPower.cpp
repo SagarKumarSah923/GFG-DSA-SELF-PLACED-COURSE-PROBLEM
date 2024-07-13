fca7f918b7e0c76c544ee28ecb19c02a9948b920
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:06:16 +0530

    Update ComputingPower.cpp

--- ComputingPower.cpp
+++ ComputingPower.cpp
@@ -1,2 +1,24 @@
-#include<isotream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
+
+ int power(int x, int n)
+{
+	if(n == 0)
+		return 1;
+
+	int temp = power(x, n/2);
+
+	temp = temp * temp;
+
+	if(n % 2 == 0)
+		return temp;
+	else
+		return temp * x; 
+}
+int main() {
+    
+    int x = 3, n = 5;
+
+	cout<<power(x, n);
+}
