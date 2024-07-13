461f80b896f76a8d75f4b5087ff2577fda2dfcdc
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:04:10 +0530

    Update All divisors of a number.cpp

--- All divisors of a number.cpp
+++ All divisors of a number.cpp
@@ -1,2 +1,28 @@
 #include<isotream>
 using namespace std;
+
+
+
+ void printDivisors(int n)
+{
+	for(int i=1; i*i <= n; i++)
+	{
+		if(n % i == 0)
+		{
+			cout<<i<<" ";
+
+        
+			if(i != (n / i))
+				cout<<(n / i)<<" ";					
+		}
+	}
+}
+
+int main() {
+    
+    	int n = 25;
+    	
+    	printDivisors(n);
+    	
+    	return 0;
+}
