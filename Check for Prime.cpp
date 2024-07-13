8466dcd38885f95f687d8c45a4066de5de2dfbb5
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-13 15:05:30 +0530

    Update Check for Prime.cpp

--- Check for Prime.cpp
+++ Check for Prime.cpp
@@ -1,2 +1,60 @@
-#include<isotream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
+
+ bool isPrime(int n)
+{
+	if(n==1)
+		return false;
+
+	for(int i=2; i*i<=n; i++)
+	{
+		if(n % i == 0)
+			return false; 
+	}
+
+	return true;
+}
+
+int main() {
+    
+    	int n = 65;
+    	
+    	printf("%s", isPrime(n) ? "true" : "false");
+    	
+    	return 0;
+}
+
+//More Efficient Code for large Number
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+ bool isPrime(int n)
+{
+	if(n==1)
+		return false;
+
+	if(n==2 || n==3)
+		return true;
+
+	if(n%2==0 || n%3==0)
+		return false;
+
+	for(int i=5; i*i<=n; i=i+6)
+	{
+		if(n % i == 0 || n % (i + 2) == 0)
+			return false; 
+	}
+
+	return true;
+}
+
+int main() {
+    
+    	int n = 1031;
+    	
+    	printf("%s", isPrime(n) ? "true" : "false");
+    	
+    	return 0;
+}
