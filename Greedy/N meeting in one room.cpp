06c5a47c0a45d32a2e6cbdb0a2088be0ab375ee7
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-14 17:18:02 +0530

    Add files via upload

---
+++ Greedy/N meeting in one room.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+bool sortbysec(const pair<int, int> &a,const pair<int,int> return (a.second < b.second));
+{
+class Solution
+{
+    public:
+    int maxMeetings(int start[],int end[],int n)
+}
+
+}
+
+int main(){
+
+}
