09588635e01613a978a1d31a43da5509d7c9894c
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-15 14:58:23 +0530

    Update SearchinRowwise and columnmatrixsortedarrays.cpp

--- Matrix/SearchinRowwise and columnmatrixsortedarrays.cpp
+++ Matrix/SearchinRowwise and columnmatrixsortedarrays.cpp
@@ -1,5 +1,90 @@
-#include<iostream>
+//Naive Approach
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h> 
+#include <climits>
+#include <deque>
 using namespace std;
-int main(){
-    return 0;
+
+const int R = 4, C = 4;
+
+void search(int mat[R][C], int x)
+{
+	for(int i = 0; i < R; i++)
+	{
+		for(int j = 0; j < C; j++)
+		{
+			if(mat[i][j] == x)
+			{
+				cout << "Found at (" << i << ", " << j << ")";
+				
+				return;
+			}
+		}
+	}
+
+	cout << "Not Found";
+}
+
+int main()
+{	
+    int arr[][C] = {{10, 20, 30, 40},
+    			   {15, 25, 35, 45},
+    			   {27, 29, 35, 45},
+    			   {32, 33, 39, 50}};
+			   
+	int x = 29;	   
+
+	search(arr, x);
+
+	return 0;
+}
+
+//Efficient Approach
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h> 
+#include <climits>
+#include <deque>
+using namespace std;
+
+const int R = 4, C = 4;
+
+void search(int mat[R][C], int x)
+{
+	int i  = 0, j = C - 1;
+
+	while(i < R && j >= 0)
+	{
+		if(mat[i][j] == x)
+		{
+			cout << "Found at (" << i << ", " << j << ")";
+			
+			return;
+		}
+		else if(mat[i][j] > x)
+		{
+			j--;
+		}
+		else
+		{
+			i++;
+		}
+	}
+		
+	cout << "Not Found";
+}
+
+int main()
+{	
+    int arr[][C] = {{10, 20, 30, 40},
+    			   {15, 25, 35, 45},
+    			   {27, 29, 35, 45},
+    			   {32, 33, 39, 50}};
+			   
+	int x = 29;	   
+
+	search(arr, x);
+
+	return 0;
 }
