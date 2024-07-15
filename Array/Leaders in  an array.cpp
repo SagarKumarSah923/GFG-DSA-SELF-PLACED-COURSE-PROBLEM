fe970e7e07c1c4a855522a14c73984bc1b9c8e63
Sagar kumar Sah <152137743+SagarKumarSah923@users.noreply.github.com>
2024-07-15 13:23:47 +0530

    Update Leaders in  an array.cpp

--- Array/Leaders in  an array.cpp
+++ Array/Leaders in  an array.cpp
@@ -1,4 +1,74 @@
+//Naive Code
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+
+void leaders(int arr[], int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		bool flag = false;
+
+		for(int j = i + 1; j < n; j++)
+		{
+			if(arr[i] <= arr[j])
+			{
+				flag = true;
+				break;
+			}
+		}
+
+		if(flag == false)
+		{
+			cout<<arr[i]<<" ";
+		}
+	}
+}
+
+//Efficient Code
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+void leaders(int arr[], int n)
+{
+	int curr_ldr = arr[n - 1];
+
+	cout<<curr_ldr<<" ";
+
+	for(int i = n - 2; i >= 0; i--)
+	{
+		if(curr_ldr < arr[i])
+		{
+			curr_ldr = arr[i];
+
+			cout<<curr_ldr<<" ";
+		}
+	}
+}
+
+
+
+int main() {
+	
+      int arr[] = {7, 10, 4, 10, 6, 5, 2}, n = 7;
+
+      leaders(arr, n);
+    
+}
+
+
+
+
+int main() {
+	
+      int arr[] = {7, 10, 4, 10, 6, 5, 2}, n = 7;
+
+      leaders(arr, n);
+    
+}
 
 
 
